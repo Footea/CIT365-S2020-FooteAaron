9f1114d91f6e25241762d4864e60d0524b6202c3
aaron-foote <35173201+Footea@users.noreply.github.com>
2020-04-23 19:31:16 -0700

    Adding Console Aplication week 1

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Output two WriteLine statements that dispaly two variables
+            //variables
+            string name = "Aaron Foote";
+            string location = "Arizona";
+
+            //WriteLine outputs
+            Console.WriteLine("My name is " + name + ".");
+            Console.WriteLine("I am from " + location + "!");
+
+
+            //Current date, no time
+            DateTime thisDay = DateTime.Today;
+
+            //WriteLine output today's date
+            Console.WriteLine();
+            Console.WriteLine(thisDay.ToString("D"));
+
+
+            //number of days till xmas
+            DateTime today = DateTime.Today;
+            DateTime xmasDay =new DateTime(DateTime.Today.Year, 12, 25);
+
+            //compute days until christmas
+            int daysLeft = xmasDay.DayOfYear - DateTime.Today.DayOfYear;
+
+            //WriteLine output day's to xmas
+            Console.WriteLine();
+            Console.WriteLine("There are " + daysLeft + " till Christmas!");
+            
+
+
+
+
+        }
+    }
+}
